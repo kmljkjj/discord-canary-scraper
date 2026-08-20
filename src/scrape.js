99ef3a6c2b34bd43fe0414c2d939061ac93b1a56
kmljkjj <jbouzeau7@icloud.com>
2026-08-20 02:43:59 +0200

    Restore scrape.js (UI-experiment linking) via loader + parts

--- src/scrape.js
+++ src/scrape.js
@@ -1,1 +1,12 @@
-PLACEHOLDER
+const fs = require('fs');
+const path = require('path');
+const parts = ['scrape.part0.js', 'scrape.part1.js', 'scrape.part2.js'];
+let code = '';
+for (const p of parts) {
+  code += fs.readFileSync(path.join(__dirname, p), 'utf8');
+}
+const Module = require('module');
+const m = new Module(path.join(__dirname, 'scrape.js'));
+m.filename = path.join(__dirname, 'scrape.js');
+m.paths = Module._nodeModulePaths(__dirname);
+m._compile(code, m.filename);
